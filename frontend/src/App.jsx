b8d484242a48fada80ef0a7ba6e2ecc0dddd524a
toicusin3 <mr.rakenraj@gmail.com>
2025-10-07 13:21:46 +0700

    create userlayout (navbar header)

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,17 @@
 import React from "react";
 import "./index.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import UserLayout from "./components/Layout/UserLayout";
 
 const App = () => {
   return (
-    <div>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<UserLayout />}>
+
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 };
 
